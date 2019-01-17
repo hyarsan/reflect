ca985826becc2fee6ee9260b95204f4184c2abcf
BuildTools <whiskerdev@protonmail.com>
2019-01-17 01:21:04 +0000

    forgot to readd notice

--- types.go
+++ types.go
@@ -1,12 +1,20 @@
 /*
 
-types.go -
-the types used in the bot
+    reflect - link discord servers together like never before
+    Copyright (C) 2018  superwhiskers <whiskerdev@protonmail.com>
 
-credits:
-  - @hyarsan#3653 - original bot creator
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-license: gnu agplv3
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 
