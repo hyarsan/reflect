3b397f3d6d92e3b72b9a4c139fad0ab83670ab75
superwhiskers <whiskerdev@protonmail.com>
2019-02-01 18:23:56 -0600

    code update

--- reflect.go
+++ reflect.go
@@ -1,20 +1,12 @@
 /*
 
-    reflect - link discord servers together like never before
-    Copyright (C) 2018  superwhiskers <whiskerdev@protonmail.com>
+reflect.go -
+an implementation of the reflect bot in golang
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+credits:
+  - @hyarsan#3653 - original bot creator
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+license: gnu agplv3
 
 */
 
@@ -117,7 +109,7 @@ func main() {
 
 	mentionRegex = regexp.MustCompile("\\@everyone|\\@here")
 
-	escapeRegex = regexp.MustCompile("\\`|\\*|\\_|\\\\")
+	escapeRegex = regexp.MustCompile("\\`|\\*|\\_|\\||\\\\")
 
 	handler = harmony.New("r~", true)
 
